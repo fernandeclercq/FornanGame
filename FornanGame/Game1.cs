f4a0573d1a01e252b0fea61ffff69af579d56651
Fernando <fernandomedinadb@hotmail.com>
2020-10-07 01:03:59 +0200

    crude animation reached

--- FornanGame/Game1.cs
+++ FornanGame/Game1.cs
@@ -1,31 +1,45 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Threading;
 
 namespace FornanGame
 {
     public class Game1 : Game
     {
+        private const string ASSET_NAME_SPRITESHEET = "CharacterSpriteSheet_4";
+        private Texture2D _texture;
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        Rectangle _partSpriteSheet;
+        Vector2 drawingPoint;
+
+        int Y_position = 0; //vertical line
+        int X_position = 0; //horizontal line
+        int width = 50;
+        int height = 75;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            Window.AllowUserResizing = true;
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            _partSpriteSheet = new Rectangle(X_position, Y_position, width, height);
+            drawingPoint = new Vector2(0, 0);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _texture = Content.Load<Texture2D>(ASSET_NAME_SPRITESHEET);
 
             // TODO: use this.Content to load your game content here
         }
@@ -37,14 +51,32 @@ namespace FornanGame
 
             // TODO: Add your update logic here
 
+            X_position += 50;
+
+            if (X_position > 200)
+            {
+                X_position = 0;
+            }
+            _partSpriteSheet.X = X_position;
+
             base.Update(gameTime);
+
+            
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Cyan);
 
             // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(_texture, drawingPoint, _partSpriteSheet, Color.White);
+
+            
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
