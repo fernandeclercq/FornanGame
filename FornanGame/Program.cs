a1251c2bb2011d4cec2848b1fafe6d673b13c82e
Fernando <fernandomedinadb@hotmail.com>
2020-10-06 19:52:18 +0200

    Add project files.

---
+++ FornanGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FornanGame
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
